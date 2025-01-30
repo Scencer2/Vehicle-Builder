f72c5c4bc3db1af076fde716e93bf6b3487f26fc
Scencer2 <Scencer2@yahoo.com>
2025-01-30 01:35:13 -0800

    added statments to create truck and motorbike

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -69,6 +69,14 @@ class Cli {
           // create a car
           this.createCar();
         }
+        else if (answers.vehicleType === 'Truck') {
+          // create a truck
+          this.createTruck();
+        }
+        else if (answers.vehicleType === 'Motorbike') {
+          // create a Motorbike
+          this.createMotorbike();
+        }
         // TODO: add statements to create a truck or motorbike if the user selects the respective vehicle type
       });
   }
